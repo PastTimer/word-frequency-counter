1e7a45fa5831f8997e08e27474904382cd2506a4
Imee <vitoimee@gmail.com>
2025-02-11 00:13:45 +0800

    Tokenization and php post implimented

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
 <body>
     <h1>Word Frequency Counter</h1>
     
-    <form action="process.php" method="post">
+    <form action="" method="post">
         <label for="text">Paste your text here:</label><br>
         <textarea id="text" name="text" rows="10" cols="50" required></textarea><br><br>
         
@@ -24,5 +24,19 @@
         
         <input type="submit" value="Calculate Word Frequency">
     </form>
+
+    <?php 
+    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+        require 'word_frequency.php';
+        $wordFrequency = new WordFrequency();
+        $wordFrequency->setSort($_POST['sort']);
+        $wordFrequency->setLimit($_POST['limit']);
+        $wordFrequency->setText($_POST['text']);
+
+        $words = preg_split('/\s+|[^a-zA-Z0-9]+/', $_POST['text']);
+        $wordFrequency->setWords($words);
+    }
+
+    ?>
 </body>
 </html>
